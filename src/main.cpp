b741d73c083d233021c45a1db11bf6fde7dd3a80
Ostrikov Serhii <serostrikov@gmail.com>
2021-11-17 12:59:16 +0300

    Added win checker, map clean, winner display at windows title

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,5 @@
 #include "SFML/Graphics.hpp"
-
+#include <sstream>
 namespace game
 {
     const uint8_t width = 3, height = 3;
@@ -20,6 +20,8 @@ namespace game
 
     struct World
     {
+        
+        CellType* lastPlaced;
         Cell** grid;
     };
 
@@ -38,6 +40,7 @@ namespace game
                 world.grid[i][j].type = &cellNone; 
             }
         }
+        world.lastPlaced = &cellCross;
 
         return world;
     }
@@ -78,12 +81,86 @@ namespace game
         cellCross.texture.loadFromFile(crossPath);
     }
 
-    void setWorldCell(World* world, uint32_t x, uint32_t y, CellType* type)
+    void swapCells(World* world)
     {
-        world->grid[x][y].type = type;
+        if(world->lastPlaced == &cellNought)
+            world->lastPlaced = &cellCross;
+        else
+            world->lastPlaced = &cellNought;
+    }
+
+    Cell* getCellAt(World* world, uint32_t x, uint32_t y)
+    {
+        return &world->grid[x][y];
+    }
+
+    bool isCellInWorld(World* world, uint32_t x, uint32_t y)
+    {
+        return (x >= 0) && (x < width) && (y >= 0) && (y < height);
+    }
+
+    CellType* checkWin(World* world, uint32_t x, uint32_t y)
+    {
+        CellType* type = getCellAt(world, x, y)->type;
+        uint32_t max_count = 1;
+        for(int32_t i = -1; i < 2; i++)
+        {
+            for(int32_t j = -1; j < 2; j++)
+            {
+                if(isCellInWorld(world, x + i, y + j) && (i != 0 || j != 0))
+                {
+                    uint32_t count = 1;
+                    int32_t dx=i, dy=j;
+                    while(isCellInWorld(world, x+dx, y+dy) && getCellAt(world, x+dx, y+dy)->type == type){
+                        count++;
+                        dx+=i;
+                        dy+=j;
+                    }
+                    dx=-i, dy=-j;
+                    while(isCellInWorld(world, x+dx, y+dy) && getCellAt(world, x+dx, y+dy)->type == type){
+                        count++;
+                        dx+=-i;
+                        dy+=-j;
+                        
+                    }
+                    if(count > max_count)
+                        max_count = count;
+                }
+            }
+        }
+        if(max_count >= 3)
+            return type;
+        return &cellNone;
+    }
+
+    
+
+    CellType* setWorldCell(World* world, uint32_t x, uint32_t y)
+    {
+        if(world->grid[x][y].type == &cellNone){
+            world->grid[x][y].type = world->lastPlaced;
+            CellType* winner = checkWin(world, x, y);
+            swapCells(world);
+            return winner;
+        }
+        return &cellNone;
+    }
+
+    void cleanMap(World* world)
+    {
+        for(uint32_t i = 0; i < width; i++)
+            for(uint32_t j = 0; j < height; j++)
+                world->grid[i][j].type = &cellNone;        
+    }
+
+    void setWinner(CellType* winner, sf::RenderWindow* window)
+    {
+        std::stringstream ss;
+        ss << "The last winner is: " << ((winner == &cellNought)? "NOUGHT" : "CROSS") << "!";
+        window->setTitle(ss.str());
     }
 }
-#include <iostream>
+
 int main()
 {
     sf::RenderWindow window(sf::VideoMode(512, 512), "Tic Tac Toe", sf::Style::Default);
@@ -105,7 +182,11 @@ int main()
                 sf::Vector2i mPos = sf::Mouse::getPosition(window);
                 sf::Vector2f worldIndex = sf::Vector2f(((float)mPos.x / (float)window.getSize().x) * game::width, 
                                                        ((float)mPos.y / (float)window.getSize().y) * game::height);
-                setWorldCell(&world, worldIndex.x, worldIndex.y, &game::cellCross);
+                game::CellType* winner = setWorldCell(&world, worldIndex.x, worldIndex.y);
+                if(winner != &game::cellNone){
+                    game::setWinner(winner, &window);
+                    game::cleanMap(&world);
+                }
             }
             
         }
